21cb83948cee5f87853b5b8fcee996ca9eee2a8a
Diwasa <39974383+Diwasa@users.noreply.github.com>
2018-06-06 15:50:37 +0800

    change match key car length

--- matchkey/gzk_match_funs.js
+++ matchkey/gzk_match_funs.js
@@ -22,27 +22,88 @@ function doMatchProc(strMatchSource) {
 
             var strLineSourceTrim = jsonItemInfo.strLineSource.replace(/,/g,"").replace(/，/g,"");
             var arrayMatchAreaFromTo = doMatchAreaGroup(strLineSourceTrim);
-            jsonItemInfo.arrayAreaGroup = arrayMatchAreaFromTo;
+
+            jsonItemInfo.areaInfo = {};
+            jsonItemInfo.areaInfo.arrayAreaGroup = arrayMatchAreaFromTo;
+            jsonItemInfo.areaInfo.areaFrom={};
+            jsonItemInfo.areaInfo.areaTo={};
             if(arrayMatchAreaFromTo.length>=2){
-                jsonItemInfo.areaFrom=arrayMatchAreaFromTo[0];
-                jsonItemInfo.areaTo=arrayMatchAreaFromTo[arrayMatchAreaFromTo.length-1];
+                jsonItemInfo.areaInfo.areaFrom.areaSource=arrayMatchAreaFromTo[0];
+                jsonItemInfo.areaInfo.areaTo.areaSource=arrayMatchAreaFromTo[arrayMatchAreaFromTo.length-1];
             }
 
             var arrayMatchGoodsName = doMatchGoodsName(strLineSourceTrim);
             jsonItemInfo.arrayGoodsName = arrayMatchGoodsName;
 
+            var arrayMatchGoodsWeight = doMatchGoodsWeight(strLineSourceTrim);
+            jsonItemInfo.arrayGoodsWeight = arrayMatchGoodsWeight;
+
             var arrayMatchCarLength = doMatchCarLength(strLineSourceTrim);
             jsonItemInfo.arrayCarLength = arrayMatchCarLength;
+
+            var arrayMatchTypeLength = doMatchCarType(strLineSourceTrim);
+            jsonItemInfo.arrayCarType = arrayMatchTypeLength;
 
             jsonMatch.matchAreaLine.push(jsonItemInfo);
         }
 
     }
 
+    jsonMatch.matchLessInfo = doReplaceMatchInfo(strMatchSource, arrayMatchAreaLine);
+
     doShowDebugLog("jsonMatch",doJsonToStrFormat(jsonMatch));
+    //$("#textMatch").val(doJsonToStrFormat(jsonMatch));
+    $("#textMatch").val(doMatchResultFormat());
 }
 
+function doMatchResultFormat() {
+    var strReturn = "";
+    for (var intTempIndex=0; intTempIndex<jsonMatch.matchAreaLine.length; intTempIndex++){
+        var infoTemp = jsonMatch.matchAreaLine[intTempIndex];
 
+        strReturn += infoTemp.strLineSource;
+        strReturn += ("\r\n");
+
+        strReturn += (infoTemp.areaInfo.areaFrom.areaSource);
+        strReturn += (" -> ");
+        strReturn += (infoTemp.areaInfo.areaTo.areaSource);
+        //strReturn += ("\r\n");
+
+        var strGoodsInfo = "";
+        if (infoTemp.arrayGoodsName.length>0){
+            strGoodsInfo += infoTemp.arrayGoodsName;
+        }
+        if (infoTemp.arrayGoodsWeight.length>0){
+            strGoodsInfo += infoTemp.arrayGoodsWeight;
+        }
+        if (strGoodsInfo!=""){
+            //strReturn += (strGoodsInfo+"\r\n");
+            strReturn += ("; "+strGoodsInfo);
+        }
+
+        var strCarInfo = "";
+        if (infoTemp.arrayCarLength.length>0){
+            strCarInfo += infoTemp.arrayCarLength;
+        }
+        if (infoTemp.arrayCarType.length>0){
+            strCarInfo += infoTemp.arrayCarType;
+        }
+        if (strCarInfo!=""){
+            //strReturn += ("需"+strCarInfo+"\r\n");
+            strReturn += ("; 需"+strCarInfo);
+        }
+
+
+        strReturn += ("\r\n\r\n");
+
+    }
+    strReturn += ("电话："+jsonMatch.matchPhone);
+    strReturn += ("\r\n");
+    strReturn += ("其他："+jsonMatch.matchLessInfo);
+    strReturn += ("\r\n");
+
+    return strReturn;
+}
 
 function doMatchPhone(strWithSource) {
     var exp = new
@@ -72,9 +133,35 @@ function doMatchGoodsName(strWithSource) {
     return (arrayMatch==null?[]:arrayMatch);
 }
 
+function doMatchGoodsWeight(strWithSource) {
+    var exp = new
+        RegExp(json_FMMatchKey.goodsWeight.strExp, "gim");
+    var arrayMatch = strWithSource.match(exp);
+    return (arrayMatch==null?[]:arrayMatch);
+}
+
 function doMatchCarLength(strWithSource) {
     var exp = new
         RegExp(json_FMMatchKey.carLength.strExp, "gim");
     var arrayMatch = strWithSource.match(exp);
     return (arrayMatch==null?[]:arrayMatch);
+}
+
+function doMatchCarType(strWithSource) {
+    var exp = new
+        RegExp(json_FMMatchKey.carType.strExp, "gim");
+    var arrayMatch = strWithSource.match(exp);
+    return (arrayMatch==null?[]:arrayMatch);
+}
+
+function doReplaceMatchInfo(strWithSource, arrayMatchInfo) {
+    if (arrayMatchInfo!=null){
+        for (var intTempIndex=0; intTempIndex<arrayMatchInfo.length; intTempIndex++){
+            strWithSource = strWithSource.replace(arrayMatchInfo[intTempIndex],"");
+        }
+        return strWithSource.replace(/\n{1}/ig,"");
+    }else{
+        return "";
+    }
+
 }
